06d9fad53c21b173c1de4e970b604465b17901dd
vens <2319687449@qq.com>
2019-09-07 12:05:01 +0800

    swagger

--- src/main/java/com/luzhiqing/bamboo/remote/client/ExpressOrderRemote.java
+++ src/main/java/com/luzhiqing/bamboo/remote/client/ExpressOrderRemote.java
@@ -1,7 +1,7 @@
 package com.luzhiqing.bamboo.remote.client;
 
 import com.luzhiqing.bamboo.remote.dto.ExpressOrderDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
